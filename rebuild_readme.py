75c7ff370217cbf7aab06161c55f867bdb38fe14
p1ratrulezzz <git@p1ratrulezzz.me>
2021-07-18 00:43:00 +0300

    Sorting

--- rebuild_readme.py
+++ rebuild_readme.py
@@ -1,6 +1,7 @@
 from src.parser.nrjparser import NRJParser
 from src.storage.jsonstorage import JsonStorage
 import constants
+from operator import itemgetter
 
 parser = NRJParser()
 
@@ -11,12 +12,13 @@ storage = JsonStorage(filename = constants.JSON_FILENAME)
 
 table = '|Position|Track|\n'
 table += '|---|--------|\n'
-for trackInfo in storage.getTracks().values():
+tracks_sorted = sorted(storage.getTracks().values(), key=itemgetter('position_avg'))
+for trackInfo in tracks_sorted:
     trackname = str(trackInfo.get('artist')) + ' - ' + trackInfo['title']
     table += '|{position}|{trackname}|\n'.format(position = int(trackInfo['position_avg']), trackname = trackname)
 
 with open('template/readme_top.md') as readme_top_fp:
-    with open('README.md', 'w') as fp:
+    with open('README.md', 'w', encoding='utf8') as fp:
         fp.write(readme_top_fp.read())
         fp.write('\n')
         fp.write(table)
