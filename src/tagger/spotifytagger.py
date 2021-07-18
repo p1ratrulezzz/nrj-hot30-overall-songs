adf09acf9c640c95f8c06dfa125db0d6b4351786
p1ratrulezzz <git@p1ratrulezzz.me>
2021-07-18 14:11:21 +0300

    Better tagger

--- src/tagger/spotifytagger.py
+++ src/tagger/spotifytagger.py
@@ -12,22 +12,31 @@ class SpotifyTagger():
 
             # Do not process if there is already a spotify info
             if trackInfo['spotify'].get('track') is not None:
-                continue
+                pass
 
-            q = ''
+            artist = ''
             if trackInfo.get('artist'):
-                q += trackInfo['artist'] + ' - '
-            q += trackInfo['title']
+                artist = trackInfo['artist'] + ' - '
 
-            try:
-                results = self._sp.search(q, limit=3, market='RU')
-            except Exception:
-                continue
+            for tries in range(0, 2):
+                q = artist
+                q += trackInfo['title']
 
-            if results.get('tracks') and results['tracks'].get('items'):
-                trackItem = results['tracks']['items'][0]
-                trackInfo['spotify']['track'] = trackItem['uri']
-                trackInfo['spotify']['album'] = trackItem['album']['uri']
-                trackInfo['spotify']['image'] = trackItem['album']['images'][0]['url']
-            else:
+                try:
+                    results = self._sp.search(q, limit=3, market='RU')
+                except Exception:
+                    continue
+
+                if results.get('tracks') and results['tracks'].get('items'):
+                    trackItem = results['tracks']['items'][0]
+                    trackInfo['spotify']['track'] = trackItem['uri']
+                    trackInfo['spotify']['album'] = trackItem['album']['uri']
+                    trackInfo['spotify']['image'] = trackItem['album']['images'][0]['url']
+                elif trackInfo.get('artist'):
+                        artist = str(trackInfo['artist'])
+                        lastSpacePos = artist.rfind(' ')
+                        if lastSpacePos != -1:
+                            artist = artist[lastSpacePos + 1:] + ' - '
+
+            if trackInfo['spotify'].get('track') is None:
                 trackInfo['spotify']['track'] = ''
