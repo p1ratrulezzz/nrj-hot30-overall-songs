8329c5a983e54679a292726514b935e69be45224
P1ratRuleZZZ <p1ratrulezzz@users.noreply.github.com>
2021-12-11 00:13:17 +0300

    Update rebuild_readme.py

--- rebuild_readme.py
+++ rebuild_readme.py
@@ -32,8 +32,8 @@ for trackInfo in tracks_sorted:
         
     change_icon = ''
     _positions = trackInfo['positions']
-    current_position = trackInfo['positions'][-1]
     if len(_positions) > 1:
+        current_position = trackInfo['positions'][-1]['value']
         delta = _positions[-1]['value'] - _positions[-2]['value']
         if delta != 0:
             change_icon = '🔻' if delta < 0 else '⬆'
